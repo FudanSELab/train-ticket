af75e036a30f92d2785a8b3a79b79ee536420ae6
PhysicalElective2 <shaoxi277214@foxmail.com>
2019-11-24 19:51:50 +0800

    一些别的test

---
+++ ts-travel2-service/src/main/test/travelto/init/InitDataTest.java
@@ -0,0 +1,67 @@
+package travelto.init; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* InitData Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>11ÔÂ 24, 2019</pre> 
+* @version 1.0 
+*/ 
+public class InitDataTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: buildTravelInfo(String... args) 
+* 
+*/ 
+@Test
+public void testBuildTravelInfo() throws Exception { 
+//TODO: Test goes here...
+    InitData id =new InitData();
+    //InitData.str2Date("EEE MMM ddHH:mm:ss 'GMT+0800' yyyy");
+} 
+
+/** 
+* 
+* Method: run(String... args) 
+* 
+*/ 
+@Test
+public void testRun() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: str2Date(String s) 
+* 
+*/ 
+@Test
+public void testStr2Date() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = InitData.getClass().getMethod("str2Date", String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
