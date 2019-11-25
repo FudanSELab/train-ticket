b66788bc2dc29730d4d383a21c5db59cdf8b3563
austin@wang <15302010039@fudan.edu.cn>
2019-11-25 19:53:31 +0800

    repair the origin

---
+++ ts-order-other-service/src/main/java/other/service/OrderOtherServiceImplTest.java
@@ -0,0 +1,104 @@
+package other.service;
+
+import org.junit.jupiter.api.Test;
+import other.repository.OrderOtherRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderOtherServiceImplTest {
+
+
+
+    @Test
+    void testInit() {
+
+    }
+
+    @Test
+    void findOrderById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void initOrder() {
+    }
+
+    @Test
+    void alterOrder() {
+    }
+
+    @Test
+    void checkEnableState() {
+    }
+
+    @Test
+    void checkEnableTravel() {
+    }
+
+    @Test
+    void checkBoughtState() {
+    }
+
+    @Test
+    void queryOrders() {
+    }
+
+    @Test
+    void queryOrdersForRefresh() {
+    }
+
+    @Test
+    void queryForStationId() {
+    }
+
+    @Test
+    void saveChanges() {
+    }
+
+    @Test
+    void cancelOrder() {
+    }
+
+    @Test
+    void queryAlreadySoldOrders() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void modifyOrder() {
+    }
+
+    @Test
+    void getOrderPrice() {
+    }
+
+    @Test
+    void payOrder() {
+    }
+
+    @Test
+    void getOrderById() {
+    }
+
+    @Test
+    void checkSecurityAboutOrder() {
+    }
+
+    @Test
+    void deleteOrder() {
+    }
+
+    @Test
+    void addNewOrder() {
+    }
+
+    @Test
+    void updateOrder() {
+    }
+}
