0f77998f5d01f20afefb646c633d7e1348c03283
meixiezichuan <meixiezichuan@163.com>
2022-07-27 11:34:29 +0800

    optimize seat

--- ts-order-other-service/src/main/java/other/service/OrderOtherServiceImpl.java
+++ ts-order-other-service/src/main/java/other/service/OrderOtherServiceImpl.java
@@ -207,7 +207,6 @@ public class OrderOtherServiceImpl implements OrderOtherService {
             stationIds.add(order.getFrom());
             stationIds.add(order.getTo());
         }
-//        List<String> names = queryForStationId(stationIds, headers);
         for (int i = 0; i < orders.size(); i++) {
             orders.get(i).setFrom(stationIds.get(i * 2));
             orders.get(i).setTo(stationIds.get(i * 2 + 1));
