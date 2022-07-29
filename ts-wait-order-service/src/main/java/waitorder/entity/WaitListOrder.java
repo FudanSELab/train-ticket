0e86a10da0fc953b304dbde7f41cfd4c2e3456e6
winterStreet <luwente@163.com>
2022-07-29 17:11:04 +0800

    change to Date

--- ts-wait-order-service/src/main/java/waitorder/entity/WaitListOrder.java
+++ ts-wait-order-service/src/main/java/waitorder/entity/WaitListOrder.java
@@ -5,20 +5,21 @@ import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.Date;
 
 
 @Data
 @AllArgsConstructor
 @Entity
-@GenericGenerator(name = "jpa-uuid", strategy = "org.hibernate.id.UUIDGenerator")
+@GenericGenerator(name = "jpa-uuid", strategy ="uuid")
 public class WaitListOrder {
     @Id
     @GeneratedValue(generator = "jpa-uuid")
     @Column(length = 36)
     private String id;
 
-    private String travelDate;
-    private String travelTime;
+//    private String travelDate;
+    private Date travelTime;
     private String accountId;
     private String contactsName;
     private int contactsDocumentType;
@@ -30,8 +31,8 @@ public class WaitListOrder {
     private String fromStation;
     private String toStation;
     private String price;
-    private String waitUtilTime;
-    private String createdTime;
+    private Date waitUtilTime;
+    private Date createdTime;
 
     public WaitListOrder(){
         //Default Constructor
